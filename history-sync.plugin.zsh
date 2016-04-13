d25d497e62a0b7e7282d4a40d3a66a73f8a6d3d9
James Fraser <wulfgar.pro@gmail.com>
2016-04-13 16:50:19 +1000

    /home/james/.zsh_history_proj

--- history-sync.plugin.zsh
+++ history-sync.plugin.zsh
@@ -28,7 +28,7 @@ history-sync-push() {
     case $commit in
       [Yy]* ) 
         git commit -am $ZSH_HISTORY_PROJ && git push $ZSH_HISTORY_PROJ; 
-        if [[ $? == 1 ]] echo "$fg_bold[red] Fix your git repo..."
+        if [[ $? == 1 ]] echo "$fg_bold[red] Fix your git repo..." fi
         break;;
       [Nn]* )
         exit;;
