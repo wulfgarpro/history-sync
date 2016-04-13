ed972aac86f34333f3d7ef7950d0035ef61361a3
James Fraser <wulfgar.pro@gmail.com>
2016-04-13 16:42:22 +1000

    /home/james/.zsh_history_proj

--- history-sync.plugin.zsh
+++ history-sync.plugin.zsh
@@ -16,17 +16,20 @@ history-sync-push() {
   read name && NAME=name
 
   if [[ -n $FILE ]]; then
+    echo "gpg'ing zsh history file: $ZSH_HISTORY_FILE"
     # Encrypt history file for push
-    gpg -r $NAME --encrypt --sign --armor --output $ZSH_HISTORY_FILE_ENC $ZSH_HISTORY_FILE
+    gpg -v -r $NAME --encrypt --sign --armor --output $ZSH_HISTORY_FILE_ENC $ZSH_HISTORY_FILE
   fi
 
-  echo -n "Do you want to commit and push current local history file?"
+  echo -n "Do you want to commit and push current local history file? "
   read commit
     
   if [[ -n $commit ]]; then
     case $commit in
       [Yy]* ) 
-        git commit -am $ZSH_HISTORY_PROJ && git push $ZSH_HISTORY_PROJ; break;;
+        git commit -am $ZSH_HISTORY_PROJ && git push $ZSH_HISTORY_PROJ; 
+        if [[ $? == 1 ]] echo "$fg_bold[white] Fix your git repo...%b"
+        break;;
       [Nn]* )
         exit;;
       * )
