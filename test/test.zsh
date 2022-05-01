1f19e386ad03c5c77a1c21ad554c28f831e3320f
James Fraser <wulfgar.pro@gmail.com>
2022-05-01 13:17:07 +1000

    Added more testing

--- test/test.zsh
+++ test/test.zsh
@@ -1,48 +1,103 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 ACCESS_KEY=$1
 
+set -e
+gpg -q --quick-gen-key --yes --batch --passphrase '' $UID
+git config --global user.name "James Fraser"
+git config --global user.email "wulfgar.pro@gmail.com"
+git clone "https://$ACCESS_KEY@github.com/wulfgarpro/history-sync-test" ~/.zsh_history_proj
+set +e
+
+function success() {
+    echo $fg[green]"$1"$reset_color
+}
+
+function failure() {
+    echo $fg[red]"$1"$reset_color
+}
+
+function info() {
+    echo $fg[yellow]"$1"$reset_color
+}
+
 function check_fn_exists() {
     typeset -f "$1" >/dev/null
-    [[ $? -eq 0 ]] || {echo "FAILURE: Function '$1' missing"; exit $?}
+    [[ $? -eq 0 ]] || {failure "FAILURE: Function '$1' missing"; exit $?}
+}
+
+function check_alais_exists() {
+    alias "$1" >/dev/null
+    [[ $? -eq 0 ]] || {failure "FAILURE: Alias '$1' missing"; exit $?}
 }
 
 function check_env_exists() {
     [[ -v $1 ]]
-    [[ $? -eq 0 ]] || {echo "FAILURE: Environment variable '$1' missing"; exit $?}
+    [[ $? -eq 0 ]] || {failure "FAILURE: Environment variable '$1' missing"; exit $?}
 }
 
 function check_history() {
     grep "$1" ~/.zsh_history >> /dev/null
-    [[ $? -eq 0 ]] || {echo {"FAILURE: History did not match '$1'"; exit $?}
+    [[ $? -eq 0 ]] || {failure "FAILURE: History did not match '$1'"; exit $?}
 }
 
-# Basic setup for tests.
-gpg --quick-gen-key --yes --batch --passphrase '' $UID
-git config --global user.name "James Fraser"
-git config --global user.email "wulfgar.pro@gmail.com"
-git clone "https://$ACCESS_KEY@github.com/wulfgarpro/history-sync-test" ~/.zsh_history_proj
-[[ -d ~/.zsh_history_proj ]]
+function setup() {
+    set -e
+    [[ -d ~/.zsh_history_proj ]]
+    # Clear existing history file
+    echo -n "" > ~/.zsh_history_proj/zsh_history
+    set +e
+}
 
-echo "TEST HISTORY-SYNC FUNCTIONS EXIST"
+info "TEST HISTORY-SYNC FUNCTIONS EXIST"
 check_fn_exists _print_git_error_msg
 check_fn_exists _print_gpg_encrypt_error_msg
 check_fn_exists _print_gpg_decrypt_error_msg
 check_fn_exists _usage
 check_fn_exists history_sync_pull
 check_fn_exists history_sync_push
-echo "SUCCESS"
+success "SUCCESS"
 
-echo "TEST ENVIRONMENT VARIABLES EXIST"
-check_env_exists ZSH_HISTORY_FILE
+info "TEST HISTORY-SYNC ALIASES EXIST"
+check_alias_exists zhps
+check_alias_exists zhpl
+check_alias_exists zhsync
+success "SUCCESS"
+
+info "TEST ENVIRONMENT VARIABLES EXIST"
 check_env_exists ZSH_HISTORY_PROJ
+check_env_exists ZSH_HISTORY_FILE_NAME
+check_env_exists ZSH_HISTORY_FILE
+check_env_exists ZSH_HISTORY_FILE_ENC_NAME
 check_env_exists ZSH_HISTORY_FILE_ENC
+check_env_exists ZSH_HISTORY_FILE_DECRYPT_NAME
 check_env_exists ZSH_HISTORY_COMMIT_MSG
-echo "SUCCESS"
+success "SUCCESS"
 
-echo "TEST SYNC HISTORY"
+info "TEST SYNC HISTORY BASIC 0"
+setup
 RAND=$RANDOM
 echo "1 echo $RAND" >> ~/.zsh_history
 echo "$UID" | zhps -y && zhpl -y
 check_history "^1 echo $RAND$"
-echo "SUCCESS"
+success "SUCCESS"
+
+info "TEST SYNC HISTORY BASIC 1"
+setup
+RAND0=$RANDOM
+RAND1=$RANDOM
+RAND2=$RANDOM
+RAND3=$RANDOM
+RAND4=$RANDOM
+echo "1 echo $RAND0" >> ~/.zsh_history
+echo "2 echo $RAND1" >> ~/.zsh_history
+echo "3 echo $RAND2" >> ~/.zsh_history
+echo "4 echo $RAND3" >> ~/.zsh_history
+echo "5 echo $RAND4" >> ~/.zsh_history
+echo "$UID" | zhps -y && zhpl -y
+check_history "^1 echo $RAND0$"
+check_history "^2 echo $RAND1$"
+check_history "^3 echo $RAND2$"
+check_history "^4 echo $RAND3$"
+check_history "^5 echo $RAND4$"
+success "SUCCESS"
